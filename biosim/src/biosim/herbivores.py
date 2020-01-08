3d8a21c35d508039598593c27cfaa0d1ec4b82a6
Olav Vikøren Espenes <olaves@nmbu.no>
2020-01-08 17:59:26 +0100

    Making code to fauna/herbivores.

--- biosim/src/biosim/herbivores.py
+++ biosim/src/biosim/herbivores.py
@@ -6,22 +6,35 @@
 __author__ = ""
 __email__ = ""
 
-class Fauna:
-    Parameters = {'w_birth': (8.0, 6.0),
-                  'sigma_birth': (4.0, 5.0),
-                  'beta': (0.9, 0.75),
-                  'eta': (0.05, 0.125),
-                  'a_half': (40.0, 60.0),
-                  'w_half': (10.0, 4.0),
-                  'phi_weight': (0.1, 0.4),
-                  'mu': (0.25, 0.4),
-                  'lambda': (1.0, 1.0),
-                  'gamma': }
-
-
-dict = {'haha':(2,2), 'hehe':3}
-for ind,val in dict:
-    print ind,val
-
 class Herbivores:
+    default_h_parameters = {'w_birth': (8.0), 'sigma_birth': (4.0), 'beta': (0.9),
+                  'eta': (0.05), 'a_half': (40.0), 'phi_age': (0.2),
+                  'w_half': (10.0), 'phi_weight': (0.1), 'mu': (0.25),
+                  'lambda': (1.0), 'gamma': (0.2), 'zeta': (3.5),
+                  'xi': (1.2), 'omega': (0.4), 'F': (10.0)}
+    h_parameters = copy.deepcopy(default_h_parameters)
+
+
+@classmethod
+def set_parameters(cls, choose_parameters):
+    """
+    :param cls:
+    :param choose_parameters:
+    :return:
+    """
+    for key in choose_parameters:
+        if key in cls.h_parameters:
+            if key is 'w_birth' or 'sigma_birth' or 'a_half' or 'w_half' or 
+
+
+
+
+
+
+
+if __name__=="__main__":
+    print(h_parameters)
+
+
+
 
