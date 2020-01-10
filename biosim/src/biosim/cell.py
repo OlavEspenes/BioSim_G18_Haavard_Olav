0dbdc074dce69c0093f0152818d47baf3a1aa326
Håvard <havardei@nmbu.no>
2020-01-10 09:39:50 +0100

    Cell

---
+++ biosim/src/biosim/cell.py
@@ -0,0 +1,30 @@
+
+
+class Cell:
+    def __init__(self):
+        self.herbi = []
+        self.carni = []
+
+
+    def feeding_herbi(self):
+        pass
+    
+    def feeding_carni(self):
+        pass
+
+    def procreation(self):
+        pass
+
+
+
+class Jungle(Cell):
+    def __init__(self):
+
+    def f_jungle(self):
+
+
+class Ocean(Cell):
+    def __init__(self):
+
+
+
