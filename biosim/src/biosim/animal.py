f5ddbbf0bcb0ebb4450b8037c59352c6cde52491
Olav Vikøren Espenes <olaves@nmbu.no>
2020-01-13 16:45:08 +0100

    Finish animal.

--- biosim/src/biosim/animal.py
+++ biosim/src/biosim/animal.py
@@ -9,36 +9,29 @@ __email__ = ""
 import math
 import numpy as np
 
+
 class Animal:
 
 
-    def __init__(self, age, weight, herbi_para, carni_para):
+    def __init__(self, herbi_para, carni_para):
 
-        if weight is None:
-            self.weight = parameters['w_birth'] + np.random.normal()*parameters['sigma_birth']
-        else:
-            self.weight = weight
-
-        self.age = age
         self.herbi_para = herbi_para
         self.carni_para = carni_para
-        self.fitness = []
 
-    def weight(self):
-        return self.weight()
 
-    def fitness_herbivores(self):
+    def fitness_herbivores(self, age, weight):
         q_plus = (1 + math.e ** (self.herbi_para['phi_age']
-                                 * (self.age - self.herbi_para['a_half'])))
+                                 * (age - self.herbi_para['a_half'])))
         q_minus = (1 + math.e ** (-self.herbi_para['phi_weight']
-                                  * (self.weight - self.herbi_para['w_half'])))
+                                  * (weight - self.herbi_para['w_half'])))
         self.fitness = (q_plus * q_minus) ** -1
-        return(self.fitness)
+        return (self.fitness)
 
-    def fitness_carnivores(self):
-        q_plus = (1 + math.e ** (self.herbi_para['phi_age']
-                                 * (self.age - self.herbi_para['a_half'])))
-        q_minus = (1 + math.e ** (-self.herbi_para['phi_weight']
-                                  * (self.weight - self.herbi_para['w_half'])))
+
+    def fitness_carnivores(self, age, weight):
+        q_plus = (1 + math.e ** (self.carni_para['phi_age']
+                                 * (age - self.carni_para['a_half'])))
+        q_minus = (1 + math.e ** (-self.carni_para['phi_weight']
+                                  * (weight - self.carni_para['w_half'])))
         self.fitness = (q_plus * q_minus) ** -1
         return (self.fitness)
